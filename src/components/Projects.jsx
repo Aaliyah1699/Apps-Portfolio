22011152088818fb38521a90dd676212ca02b1fc
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-18 16:15:27 -0600

    Add projects intro

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,6 +9,12 @@ const Projects = () => {
             id='projects'
         >
             <SectionTitle text='My Projects' />
+            <p className='text-slate-50 mt-1'>
+                Explore my collection of projects, with some already deployed and
+                ready for real-world use, while others are primed for deployment
+                or integration, whether as standalone applications or seamlessly
+                connected to frontend or backend components.
+            </p>
             <div className='py-10 grid lg:grid-cols-2 xl:grid-cols-3 gap-8'>
                 {projects.map((project) => {
                     return <ProjectsCard key={project.id} {...project} />;
